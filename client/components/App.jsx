46dab6db61359e5bb7e932c488b6290e8977e557
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-24 10:28:27 +1200

    Set up initial routes and home page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+
+import Home from './Home'
+import Meditation from './Meditation'
+import MeditationHome from './MeditationHome'
+
 const App = () => {
   return (
-
-    <h1>React development has begun!</h1>
-
+    <>
+      <Route exact path ='/' component={Home} />
+      <Route exact path='/home' component={MeditationHome} />
+      <Route exact path='/meditate' component={Meditation} />
+    </>
   )
 }
 
