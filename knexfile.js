d454d34d287cabae3d1c084e2aac8d209104e208
malyz <mattuy001@gmail.com>
2020-04-23 22:00:13 +1200

    added template

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    migrations: {
+      directory: __dirname + '/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/server/db/seeds/development'
+    }
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/server/db/seeds/development'
+    }
+  }
+}
