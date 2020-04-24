a31a0d39fad9be93dab63f1295eabf787a0ac600
malyz <mattuy001@gmail.com>
2020-04-24 17:52:23 +1200

    applied redux thunk

--- client/store/actions/sessions.js
+++ client/store/actions/sessions.js
@@ -3,12 +3,16 @@ import api from '../../api'
 import { FETCH_SESSIONS, FETCH_USER_SESSIONS } from '../types'
 
 export const fetchUserSessions = (userId) => (dispatch) => {
-  api.get(`/v1/sessions/${userId}`).then((session) => {
-    dispatch({
-      type: FETCH_USER_SESSIONS,
-      payload: session
+  console.log('inside the fetchUserSessions')
+  api
+    .get(`/v1/sessions/${userId}`)
+    .then((session) => {
+      dispatch({
+        type: FETCH_USER_SESSIONS,
+        payload: session.data
+      })
     })
-  })
+    .catch((err) => console.log(err.message))
 }
 
 export const fetchSessions = () => (dispatch) => {
